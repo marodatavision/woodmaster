d61aab158a7ab2ba4fcd2f0d13b6aa70fb64d7a9
marodatavision <maxrosen926@gmail.com>
2021-08-30 22:13:36 +0200

    registered policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use App\Models\Person;
+use App\Models\SawnTimber;
+use App\Models\WoodenLog;
+use App\Policies\OrderPolicy;
+use App\Policies\PersonPolicy;
+use App\Policies\SawnTimberPolicy;
+use App\Policies\StoragePolicy;
+use App\Policies\WoodenLogPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rules\Password;
@@ -14,7 +23,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Order::class => OrderPolicy::class,
+        Person::class => PersonPolicy::class,
+        SawnTimber::class => SawnTimberPolicy::class,
+        Storage::class => StoragePolicy::class,
+        WoodenLog::class => WoodenLogPolicy::class
     ];
 
     /**
