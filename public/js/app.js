3beed139d3ffb41ffe1b285119e2dc1a288a37f7
marodatavision <maxrosen926@gmail.com>
2021-09-12 15:08:30 +0200

    added table with on hover style

--- public/js/app.js
+++ public/js/app.js
@@ -20130,6 +20130,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _material_ui_styles__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @material-ui/styles */ "./node_modules/@material-ui/styles/esm/withStyles/withStyles.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/esm/react-router.js");
 /* harmony import */ var _config_baseurls__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../config/baseurls */ "./resources/js/src/config/baseurls.js");
@@ -20151,7 +20152,19 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
+ // styles
 
+
+
+var listItemStyle = {
+  tr: {
+    // backgroundColor: "#f1f1f1",
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: '#6AB9FA'
+    }
+  }
+};
 
 var WoodenLogView = function WoodenLogView(props) {
   // url params
@@ -20168,16 +20181,54 @@ var WoodenLogView = function WoodenLogView(props) {
     console.log(orderId);
     _services_BaseApi__WEBPACK_IMPORTED_MODULE_2__.default.show(_config_baseurls__WEBPACK_IMPORTED_MODULE_1__.WOODEN_LOG_ORDER_API_BASE, setWoodenLogs, orderId);
   }, []);
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
-    children: woodenLogs ? woodenLogs.map(function (wl, i) {
-      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
-        children: wl.id
-      });
-    }) : null
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("table", {
+      className: "table",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("thead", {
+        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("tr", {
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+            scope: "col",
+            children: "Typ"
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+            scope: "col",
+            children: "Durchmesser"
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+            scope: "col",
+            children: "L\xE4nge"
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+            scope: "col",
+            children: "Qualit\xE4t"
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+            scope: "col",
+            children: "Form"
+          })]
+        })
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("tbody", {
+        children: woodenLogs ? woodenLogs.map(function (wl, i) {
+          return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("tr", {
+            className: props.classes.tr,
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("th", {
+              scope: "row",
+              children: wl.tree_type
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+              children: wl.diameter
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+              children: wl.length
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+              children: wl.quality
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("td", {
+              children: wl.shape
+            })]
+          });
+        }) : null
+      })]
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("ul", {
+      className: "list-group"
+    })]
   });
 };
 
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (WoodenLogView);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,_material_ui_styles__WEBPACK_IMPORTED_MODULE_5__.default)(listItemStyle)(WoodenLogView));
 
 /***/ }),
 
--- resources/js/src/components/wooden-log-components/WoodenLogView.js
+++ resources/js/src/components/wooden-log-components/WoodenLogView.js
@@ -1,7 +1,19 @@
+import { withStyles } from '@material-ui/styles';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { WOODEN_LOG_ORDER_API_BASE } from '../../config/baseurls';
 import BaseAPI from '../../services/BaseApi';
+
+// styles
+const listItemStyle = {
+    tr: {
+        // backgroundColor: "#f1f1f1",
+        cursor: 'pointer', 
+        '&:hover': {
+            backgroundColor: '#6AB9FA'
+        }
+    }
+}
 
 const WoodenLogView = (props) => {
 
@@ -18,14 +30,37 @@ const WoodenLogView = (props) => {
 
     return (
         <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Typ</th>
+                        <th scope="col">Durchmesser</th>
+                        <th scope="col">Länge</th>
+                        <th scope="col">Qualität</th>
+                        <th scope="col">Form</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        woodenLogs
+                        ? woodenLogs.map((wl, i) => (
+                            <tr className={props.classes.tr}>
+                                <th scope="row">{wl.tree_type}</th>
+                                <td>{wl.diameter}</td>
+                                <td>{wl.length}</td>
+                                <td>{wl.quality}</td>
+                                <td>{wl.shape}</td>
+                            </tr>
+                        ))
+                        : null
+                    }                   
+                </tbody>
+            </table>
+            <ul className="list-group">
             
-            {
-                woodenLogs
-                ? woodenLogs.map((wl, i) => <div>{wl.id}</div>)
-                : null
-            }
+            </ul>
         </div>
     );
 }
 
-export default WoodenLogView;
+export default withStyles(listItemStyle)(WoodenLogView);
